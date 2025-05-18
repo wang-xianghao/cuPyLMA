c4dae351fb665dd66c59c5e3645a0d1f42bb099f
Xianghao <wang.xianghao@outlook.com>
2025-05-18 19:13:05 +0000

    update matmul bench

---
+++ benchmarks/preliminary/matmul/run_matmul_100x.sh
@@ -0,0 +1,39 @@
+unset CUPYNUMERIC_FAST_MATH
+
+# Square Jacobian matrix
+# 1 GPU
+echo 
+echo '1 GPU'
+CUPYNUMERIC_FAST_MATH=1 legate --cpus 16 --gpus 1 benchmarks/preliminary/matmul/run_matmul.py \
+    --case fp32 40_00 40_0000
+legate --cpus 16 --gpus 1 benchmarks/preliminary/matmul/run_matmul.py \
+    --case fp32 40_00 40_0000 \
+    --case fp64 40_00 40_0000
+
+# 2 GPU
+echo 
+echo '2 GPU'
+CUPYNUMERIC_FAST_MATH=1 legate --cpus 16 --gpus 2 benchmarks/preliminary/matmul/run_matmul.py \
+    --case fp32 40_00 40_0000
+legate --cpus 16 --gpus 2 benchmarks/preliminary/matmul/run_matmul.py \
+    --case fp32 40_00 40_0000 \
+    --case fp64 40_00 40_0000
+
+# 4 GPU
+echo 
+echo '4 GPU'
+CUPYNUMERIC_FAST_MATH=1 legate --cpus 16 --gpus 4 benchmarks/preliminary/matmul/run_matmul.py \
+    --case fp32 40_00 40_0000
+legate --cpus 16 --gpus 4 benchmarks/preliminary/matmul/run_matmul.py \
+    --case fp32 40_00 40_0000 \
+    --case fp64 40_00 40_0000
+
+
+# 8 GPU
+echo 
+echo '8 GPU'
+CUPYNUMERIC_FAST_MATH=1 legate --cpus 16 --gpus 8 benchmarks/preliminary/matmul/run_matmul.py \
+    --case fp32 40_00 40_0000
+legate --cpus 16 --gpus 8 benchmarks/preliminary/matmul/run_matmul.py \
+    --case fp32 40_00 40_0000 \
+    --case fp64 40_00 40_0000
---
+++ benchmarks/preliminary/matmul/run_matmul_20k.sh
@@ -0,0 +1,39 @@
+unset CUPYNUMERIC_FAST_MATH
+
+# Square Jacobian matrix
+# 1 GPU
+echo 
+echo '1 GPU'
+CUPYNUMERIC_FAST_MATH=1 legate --cpus 16 --gpus 1 benchmarks/preliminary/matmul/run_matmul.py \
+    --case fp32 20_000 20_000
+legate --cpus 16 --gpus 1 benchmarks/preliminary/matmul/run_matmul.py \
+    --case fp32 20_000 20_000 \
+    --case fp64 20_000 20_000
+
+# 2 GPU
+echo 
+echo '2 GPU'
+CUPYNUMERIC_FAST_MATH=1 legate --cpus 16 --gpus 2 benchmarks/preliminary/matmul/run_matmul.py \
+    --case fp32 20_000 20_000
+legate --cpus 16 --gpus 2 benchmarks/preliminary/matmul/run_matmul.py \
+    --case fp32 20_000 20_000 \
+    --case fp64 20_000 20_000
+
+# 4 GPU
+echo 
+echo '4 GPU'
+CUPYNUMERIC_FAST_MATH=1 legate --cpus 16 --gpus 4 benchmarks/preliminary/matmul/run_matmul.py \
+    --case fp32 20_000 20_000
+legate --cpus 16 --gpus 4 benchmarks/preliminary/matmul/run_matmul.py \
+    --case fp32 20_000 20_000 \
+    --case fp64 20_000 20_000
+
+
+# 8 GPU
+echo 
+echo '8 GPU'
+CUPYNUMERIC_FAST_MATH=1 legate --cpus 16 --gpus 8 benchmarks/preliminary/matmul/run_matmul.py \
+    --case fp32 20_000 20_000
+legate --cpus 16 --gpus 8 benchmarks/preliminary/matmul/run_matmul.py \
+    --case fp32 20_000 20_000 \
+    --case fp64 20_000 20_000
