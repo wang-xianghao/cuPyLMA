e2db07ea016b36062a3d5be8d46f86a11ac4c7cd
Xianghao <wang.xianghao@outlook.com>
2025-05-22 06:19:44 +0000

    add pin memory option to communication bench

--- benchmarks/preliminary/communication/run_h2d.py
+++ benchmarks/preliminary/communication/run_h2d.py
@@ -5,11 +5,13 @@ from legate.timing import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('bytes', type=int)
+parser.add_argument('--pin', action='store_true')
 args = parser.parse_args()
 
 nbytes = args.bytes
+pin = args.pin
 
-h_data = torch.randn(nbytes // 4, dtype=torch.float32, pin_memory=True)
+h_data = torch.randn(nbytes // 4, dtype=torch.float32, pin_memory=args.pin)
 
 start = time()
 d_data = np.array(h_data, dtype=np.float32)
--- benchmarks/preliminary/communication/run_h2d.sh
+++ benchmarks/preliminary/communication/run_h2d.sh
@@ -3,6 +3,11 @@ legate --cpus 16 --gpus 1 benchmarks/preliminary/communication/run_h2d.py 2_000_
 legate --cpus 16 --gpus 1 benchmarks/preliminary/communication/run_h2d.py 4_000_000_000
 legate --cpus 16 --gpus 1 benchmarks/preliminary/communication/run_h2d.py 8_000_000_000
 
+legate --cpus 16 --gpus 1 benchmarks/preliminary/communication/run_h2d.py 1_000_000_000 --pin
+legate --cpus 16 --gpus 1 benchmarks/preliminary/communication/run_h2d.py 2_000_000_000 --pin
+legate --cpus 16 --gpus 1 benchmarks/preliminary/communication/run_h2d.py 4_000_000_000 --pin
+legate --cpus 16 --gpus 1 benchmarks/preliminary/communication/run_h2d.py 8_000_000_000 --pin
+
 legate --cpus 16 --gpus 1 benchmarks/preliminary/communication/run_h2d.py 8_000_000_000
 legate --cpus 16 --gpus 2 benchmarks/preliminary/communication/run_h2d.py 8_000_000_000
 legate --cpus 16 --gpus 3 benchmarks/preliminary/communication/run_h2d.py 8_000_000_000
