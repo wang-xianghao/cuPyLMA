22f9eff31c880f4d5ba25caeb680e816d6ea8afe
xianghao <wang.xianghao@outlook.com>
2025-09-10 22:17:15 +1000

    remove benchmarks

--- benchmarks/preliminary/communication/run_d2h.sh
+++
@@ -1,9 +0,0 @@
-python benchmarks/preliminary/communication/run_d2h.py 1_000_000_000 --pin
-python benchmarks/preliminary/communication/run_d2h.py 2_000_000_000 --pin
-python benchmarks/preliminary/communication/run_d2h.py 4_000_000_000 --pin
-python benchmarks/preliminary/communication/run_d2h.py 8_000_000_000 --pin
-
-python benchmarks/preliminary/communication/run_d2h.py 1_000_000_000
-python benchmarks/preliminary/communication/run_d2h.py 2_000_000_000
-python benchmarks/preliminary/communication/run_d2h.py 4_000_000_000
-python benchmarks/preliminary/communication/run_d2h.py 8_000_000_000
